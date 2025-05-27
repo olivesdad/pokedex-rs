dbd50dfa67dd3dfe512b622bbe81bea93d17f936
andy <ashiraki>
2025-05-26 23:43:13 -0700

    fmt

--- pokedex-rs/src/main.rs
+++ pokedex-rs/src/main.rs
@@ -1,4 +1,4 @@
-use api_calls::PokemonIdentifier;
+use api_calls::{PokeReturn, PokemonIdentifier};
 
 pub mod api_calls;
 pub mod data_structures;
@@ -14,22 +14,26 @@ async fn main() {
     let data = api_calls::get_pokemon(peet).await.unwrap();
     println!("{:#?}", data);
 
-    let image = api_calls::get_image(
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/998.png",
-    )
-    .await
-    .unwrap();
-    //println!("{:#?}", image);
+    let data = api_calls::get_pokemon(poop).await.unwrap();
+    println!("{:#?}", &data);
 
-    use viuer::{Config, print};
+    if let PokeReturn::ReturnPokemonStruct(x) = data {
+        if let Ok(image) = api_calls::get_image(
+            &x.sprites.front_default.unwrap(), //"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/998.png",
+        )
+        .await
+        {
+            use viuer::{Config, print};
 
-    let conf = Config {
-        // Start from row 4 and column 20.
-        x: 50,
-        y: 0,
-        ..Default::default()
-    };
+            let conf = Config {
+                // Start from row 4 and column 20.
+                x: 50,
+                y: 0,
+                ..Default::default()
+            };
 
-    //let img = image::DynamicImage::ImageRgba8(image::RgbaImage::new(20, 10));
-    print(&image, &conf).expect("Image printing failed.");
+            //let img = image::DynamicImage::ImageRgba8(image::RgbaImage::new(20, 10));
+            print(&image, &conf).expect("Image printing failed.");
+        }
+    }
 }
